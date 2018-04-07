f061f9d456c74a1dc4578db7e0055e0e217a8883
Ramit Surana <ramitsurana@gmail.com>
2018-04-08 02:12:29 +0530

    Create sign_up_exceptions.py

---
+++ lambda/cognito_sign_up_user/sign_up_exceptions.py
@@ -0,0 +1,13 @@
+"""Sign Up Exceptions."""
+
+
+class UsernameExistsException(Exception):
+    """User name exists exceptions."""
+
+    pass
+
+
+class InvalidPasswordException(Exception):
+    """Invalid password exception."""
+
+    pass
