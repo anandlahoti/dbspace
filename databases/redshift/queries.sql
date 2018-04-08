c5b74a51c12d279971cab2c2bc8e64bfdd23b7bf
Ramit Surana <ramitsurana@gmail.com>
2018-04-08 16:30:59 +0530

    Create queries.sql

---
+++ databases/redshift/queries.sql
@@ -0,0 +1,3 @@
+# Based on PostgreSQL 
+# Connect using Psql. Ref - https://docs.aws.amazon.com/redshift/latest/mgmt/connecting-from-psql.html
+# Ref - https://docs.aws.amazon.com/redshift/latest/dg/c_SQL_commands.html
