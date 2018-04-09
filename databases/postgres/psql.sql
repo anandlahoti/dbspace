9b98a6eab139589b55d9391cf262d312ee020979
Ramit Surana <ramitsurana@gmail.com>
2018-04-09 16:05:25 +0530

    new entries

--- databases/postgres/psql.sql
+++ databases/postgres/psql.sql
@@ -1,2 +1,13 @@
 # Describe Table
 \d+ table_name
+
+# List Tables
+\d
+
+# Alter table with a change in datatype for a column
+ALTER table table_name
+ALTER COLUMN COLUMN1 type DATATYPE;
+
+# Add column in postgres
+ALTER table table_name
+ADD COLUMN COLUMN_NAME DATATYPE;
