d604a869ba3fdf7bd797330ef6bf55a6746b6d31
Ramit Surana <ramitsurana@gmail.com>
2018-04-09 23:44:19 +0530

    add export to json

--- databases/postgres/psql.sql
+++ databases/postgres/psql.sql
@@ -11,3 +11,12 @@ ALTER COLUMN COLUMN1 type DATATYPE;
 # Add column in postgres
 ALTER table table_name
 ADD COLUMN COLUMN_NAME DATATYPE;
+
+# Export Table to JSON
+\t
+\a
+\o file.json
+SELECT row_to_json(r) FROM table_name AS r;
+
+# Check the file using
+# cat file.json on local system
